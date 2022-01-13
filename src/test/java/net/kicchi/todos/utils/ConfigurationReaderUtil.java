3f410d291d569d30855113263ee0aa722bd5088f
ececi <huseyinkiceci@gmail.com>
2022-01-13 20:37:09 +0300

    configuration reader added

--- src/test/java/net/kicchi/todos/utils/ConfigurationReaderUtil.java
+++ src/test/java/net/kicchi/todos/utils/ConfigurationReaderUtil.java
@@ -1,32 +1,29 @@
 package net.kicchi.todos.utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import net.kicchi.todos.config.ApplicationConfig;
+import net.kicchi.todos.enums.BrowserType;
+
+import java.io.File;
 import java.io.FileInputStream;
 import java.util.Properties;
 
 /**
- * reads the properties file configuration.properties
+ * reads the properties file configuration.yml
  */
 public class ConfigurationReaderUtil {
 
-    private static Properties properties;
-
-    static {
-
-        try {
-            String path = "configuration.properties";
-            FileInputStream input = new FileInputStream(path);
-            properties = new Properties();
-            properties.load(input);
-
-            input.close();
-        } catch (Exception e) {
+    public static ApplicationConfig getConfiguration(){
+        try{
+            File file = new File("src/test/resources/configuration.yml");
+            ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+            ApplicationConfig applicationConfig = objectMapper.readValue(file, ApplicationConfig.class);
+            return applicationConfig;
+        }catch (Exception e){
             e.printStackTrace();
-
+            return null;
         }
-    }
-
-    public static String get(String keyName) {
-        return properties.getProperty(keyName);
     }
 
 }
