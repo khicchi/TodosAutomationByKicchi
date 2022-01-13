8aa341fe1130be9a78797e3d62a838ef6acb27ba
ececi <huseyinkiceci@gmail.com>
2022-01-13 20:36:47 +0300

    config structure changed to ApplicationConfig class

---
+++ src/test/java/net/kicchi/todos/config/ApplicationConfig.java
@@ -0,0 +1,32 @@
+package net.kicchi.todos.config;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import net.kicchi.todos.enums.BrowserType;
+
+import java.util.Arrays;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class ApplicationConfig {
+
+    @JsonProperty("main-page-url")
+    private String mainPageUrl;
+
+    @JsonProperty("url-to-navigate")
+    private String urlToNavigate;
+
+    @JsonProperty("browser-type-id")
+    private int browserTypeId;
+
+    @JsonProperty("remote-grid-url")
+    private String remoteGridUrl;
+
+    public BrowserType getBrowserType(){
+        return BrowserType.getById(browserTypeId);
+    }
+}
